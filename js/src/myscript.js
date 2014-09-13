c790775c0d6021e8a1830e57cbe73ebd928113c5
David Collins <davidcollins4481@gmail.com>
2014-09-13 14:56:25 -0400

    .

---
+++ js/src/myscript.js
@@ -0,0 +1,19 @@
+;(function($) {
+/*
+    $(document).ready(function() {
+        var circle = $('#me > circle');
+
+        var animateCircles = function() {
+            $('#me > circle').animate(
+                { r: 100 },
+                {
+                    duration: 1000,
+                    step: function(now) { $(this).attr("r", now); }
+                }
+            );
+        };
+
+        animateCircles();
+    });
+*/
+})(jQuery);
